b74ac28f7f0c3a4716e6958d30cbf37e1dc3741c
TahirAnny <tahiraanny@gmail.com>
2023-08-31 04:08:33 -0400

    Added bootstrap, icons and displayed a list of category in the category page

--- BookVerse/Controllers/CategoryController.cs
+++ BookVerse/Controllers/CategoryController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookVerse.Data;
+using BookVerse.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookVerse.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Category> categories = _context.Categories.ToList();
+            return View(categories);
         }
     }
 }
