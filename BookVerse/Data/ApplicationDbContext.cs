773a9d323681cc165065ab9ea41f8a8004ace090
TahirAnny <tahiraanny@gmail.com>
2023-08-31 03:43:32 -0400

    Added Category model, Migration along with seed data

--- BookVerse/Data/ApplicationDbContext.cs
+++ BookVerse/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BookVerse.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookVerse.Data
 {
@@ -7,6 +8,21 @@ namespace BookVerse.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
             
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, CategoryName = "Sci-Fi", DisplayOrder = 1 },
+                new Category { Id = 2, CategoryName = "Fiction", DisplayOrder = 2 },
+                new Category { Id = 3, CategoryName = "Horror", DisplayOrder = 3 },
+                new Category { Id = 4, CategoryName = "Fantasy", DisplayOrder = 4 },
+                new Category { Id = 5, CategoryName = "Thriller", DisplayOrder = 5 },
+                new Category { Id = 6, CategoryName = "Biography", DisplayOrder = 6 },
+                new Category { Id = 7, CategoryName = "Poetry", DisplayOrder = 7 }
+                ) ;
         }
     }
 }
