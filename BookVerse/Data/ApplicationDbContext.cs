09cb2b857402fea1a5615659daa3e89dc4b09781
TahirAnny <tahiraanny@gmail.com>
2023-08-31 03:17:26 -0400

    Added Database configuration for the first time in the project

---
+++ BookVerse/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookVerse.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
