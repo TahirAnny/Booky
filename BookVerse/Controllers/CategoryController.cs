977899fabeae6186cac451d56738e69d429f1495
TahirAnny <tahiraanny@gmail.com>
2023-09-06 22:28:51 -0400

    Custom Validation added for category

--- BookVerse/Controllers/CategoryController.cs
+++ BookVerse/Controllers/CategoryController.cs
@@ -23,5 +23,17 @@ namespace BookVerse.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
--- BookVerse/Models/Category.cs
+++ BookVerse/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BookVerse.Models
 {
@@ -8,8 +9,12 @@ namespace BookVerse.Models
         public int Id { get; set; }
 
         [Required]
-        public string CategoryName{ get; set; }
+        [MaxLength(100)]
+        [DisplayName("Category Name")]
+        public string CategoryName { get; set; }
 
+        [DisplayName("Display Order")]
+        [Range(0, 100, ErrorMessage ="Display order must be between 1-100")]
         public int DisplayOrder{ get; set; }
     }
 }
