88555377d1432ebc761eb342dac33e800a0a69b8
TahirAnny <tahiraanny@gmail.com>
2023-09-11 23:37:31 -0400

    Product creation

--- Book.Models/Product.cs
+++ Book.Models/Product.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace Book.Models
 {
@@ -49,8 +50,10 @@ namespace Book.Models
 
         public int CategoryId { get; set; }
         [ForeignKey("CategoryId")]
+        [ValidateNever]
         public Category Category { get; set; }
 
+        [ValidateNever]
         public string ImgUrl { get; set; }
     }
 }
---
+++ Book.Models/ViewModels/ProductViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book.Models.ViewModels
+{
+    public class ProductViewModel
+    {
+        //[ValidateNever]
+        public Product Products { get; set; }
+
+        [ValidateNever]
+        public IEnumerable<SelectListItem> CagetoryList { get; set; }
+    }
+}
