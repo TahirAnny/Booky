2c0e80052926676759a57ba1251c8ae13adf92c1
TahirAnny <tahiraanny@gmail.com>
2023-12-21 23:25:16 -0500

    Identity Added

--- BookVerse/Program.cs
+++ BookVerse/Program.cs
@@ -2,6 +2,7 @@ using Book.DataAccessLayer.Repository.IRepository;
 using Book.DataAccessLayer.Repository.Repository;
 using BooK.DataAccessLayer.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 
 //Unit of Work Service
 builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
@@ -27,7 +30,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
