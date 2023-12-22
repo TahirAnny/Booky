70c5b547d683d14a83c18c721087a9eb173e4baf
TahirAnny <tahiraanny@gmail.com>
2023-12-21 23:28:03 -0500

    Added routing for razor page in Services

--- BookVerse/Program.cs
+++ BookVerse/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddRazorPages();
 
 //Unit of Work Service
 builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
@@ -32,7 +33,7 @@ app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
-
+app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
