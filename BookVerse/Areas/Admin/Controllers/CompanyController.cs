e9be7c40ea80d4f12009a71a606ae8bb3ab3c547
TahirAnny <tahiraanny@gmail.com>
2024-07-30 17:21:12 -0400

    Company Crud

---
+++ BookVerse/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,98 @@
+﻿using Book.DataAccessLayer.Repository.IRepository;
+using Book.Models;
+using Book.Models.ViewModels;
+using Book.Utility;
+using BooK.DataAccessLayer.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BookVerse.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class CompanyController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public CompanyController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;  
+        }
+
+        public IActionResult Index()
+        {
+            List<Company> categories = _unitOfWork.Company.GetAll().ToList();
+
+            return View(categories);
+        }
+
+        public IActionResult Upsert(int? CompanyId)
+        {
+            if (CompanyId == null || CompanyId == 0)
+            {
+                return View(new Company());
+            }
+            else
+            {
+                Company company = _unitOfWork.Company.Get(x => x.Id == CompanyId);
+                return View(company);
+            }
+
+        }
+
+        [HttpPost]
+        public IActionResult Upsert(Company Company, IFormFile? formFile)
+        {
+            if (ModelState.IsValid)
+            {
+                if(Company.Id == 0)
+                {
+                    _unitOfWork.Company.Add(Company);
+                }
+                else
+                {
+                    _unitOfWork.Company.Update(Company);
+                }
+
+                _unitOfWork.Complete();
+                TempData["success"] = "Company Saved successfully!";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(new Company());
+            }
+        }
+
+        public IActionResult Delete(int? CompanyId)
+        {
+            if (CompanyId == null || CompanyId == 0)
+            {
+                return NotFound();
+            }
+            Company? Company = _unitOfWork.Company.Get(c => c.Id == CompanyId);
+
+            if (Company == null)
+            {
+                return NotFound();
+            }
+            return View(Company);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteData(int? CompanyId)
+        {
+            Company? Company = _unitOfWork.Company.Get(c => c.Id == CompanyId);
+            if (Company == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Company.Remove(Company);
+            _unitOfWork.Complete();
+            TempData["success"] = "Company Deleted successfully!";
+            return RedirectToAction("Index");
+        }
+    }
+}
--- BookVerse/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BookVerse/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading;
 using System.Threading.Tasks;
+using Book.DataAccessLayer.Repository.IRepository;
 using Book.Models;
 using Book.Utility;
 using Microsoft.AspNetCore.Authentication;
@@ -34,6 +35,7 @@ namespace BookVerse.Areas.Identity.Pages.Account
         private readonly IUserEmailStore<IdentityUser> _emailStore;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
+        private readonly IUnitOfWork _unitOfWork;
 
         public RegisterModel(
             UserManager<IdentityUser> userManager,
@@ -41,7 +43,8 @@ namespace BookVerse.Areas.Identity.Pages.Account
             IUserStore<IdentityUser> userStore,
             SignInManager<IdentityUser> signInManager,
             ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            IEmailSender emailSender, 
+            IUnitOfWork unitOfWork)
         {
             _userManager = userManager;
             _roleManager = roleManager;
@@ -50,6 +53,7 @@ namespace BookVerse.Areas.Identity.Pages.Account
             _signInManager = signInManager;
             _logger = logger;
             _emailSender = emailSender;
+            _unitOfWork = unitOfWork;
         }
 
         /// <summary>
@@ -121,6 +125,11 @@ namespace BookVerse.Areas.Identity.Pages.Account
             public string? PostalCode { get; set; }
 
             public string? PhoneNumber { get; set; }
+
+            public int? CompanyId { get; set; }
+
+            [ValidateNever]
+            public IEnumerable<SelectListItem> ComapnyList { get; set; }
         }
 
 
@@ -148,6 +157,11 @@ namespace BookVerse.Areas.Identity.Pages.Account
                 {
                     Text = i,
                     Value = i
+                }),
+                ComapnyList = _unitOfWork.Company.GetAll().Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
                 })
             };
 
@@ -171,6 +185,11 @@ namespace BookVerse.Areas.Identity.Pages.Account
                 user.State = Input.State;
                 user.PostalCode = Input.PostalCode;
                 user.PhoneNumber = Input.PhoneNumber;
+
+                if(Input.Role == SD.Role_Company)
+                {
+                    user.CompanyId = Input.CompanyId;
+                }
                 
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
