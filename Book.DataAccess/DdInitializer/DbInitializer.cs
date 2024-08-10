2ecc8ea78a8df10e02f7aff17619885d03cb683c
TahirAnny <tahiraanny@gmail.com>
2024-08-09 22:58:57 -0400

    DB Initializer added for migration

---
+++ Book.DataAccess/DdInitializer/DbInitializer.cs
@@ -0,0 +1,75 @@
+﻿using BooK.DataAccessLayer.Data;
+using Book.Models;
+using Book.Utility;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Book.DataAccessLayer.DdInitializer
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ApplicationDbContext _db;
+
+        public DbInitializer(
+            UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            ApplicationDbContext db)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+            _db = db;
+        }
+
+
+        public void Initialize()
+        {
+            //Add initial migration if not added
+            try
+            {
+                if (_db.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _db.Database.Migrate();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            //create roles if not created
+            if (!_roleManager.RoleExistsAsync(SD.Role_Customer).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Company)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
+
+                //if roles are not created, then create admin user as well
+
+                _userManager.CreateAsync(new ApplicationUser
+                {
+                    UserName = "tahirabishwas@gmail.com",
+                    Email = "tahirabishwas@gmail.com",
+                    Name = "Tahira Anny",
+                    PhoneNumber = "123456789",
+                    StreetAddress = "Bridge Ave",
+                    State = "ON",
+                    PostalCode = "N9B 2M1",
+                    City = "Windsor"
+                }, "#Test123").GetAwaiter().GetResult();
+
+                ApplicationUser user = _db.ApplicationUsers.FirstOrDefault(u => u.Email == "tahirabishwas@gmail.com");
+                _userManager.AddToRoleAsync(user, SD.Role_Admin).GetAwaiter().GetResult();
+            }
+           
+            return;
+        }
+    }
+}
--- BookVerse/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BookVerse/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -135,22 +135,6 @@ namespace BookVerse.Areas.Identity.Pages.Account
 
         public async Task OnGetAsync(string returnUrl = null)
         {
-            if(!_roleManager.RoleExistsAsync(SD.Role_Customer).GetAwaiter().GetResult()) { 
-                _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
-            }
-            if (!_roleManager.RoleExistsAsync(SD.Role_Employee).GetAwaiter().GetResult())
-            {
-                _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
-            }
-            if (!_roleManager.RoleExistsAsync(SD.Role_Company).GetAwaiter().GetResult())
-            {
-                _roleManager.CreateAsync(new IdentityRole(SD.Role_Company)).GetAwaiter().GetResult();
-            }
-            if (!_roleManager.RoleExistsAsync(SD.Role_Admin).GetAwaiter().GetResult())
-            {
-                _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
-            }
-
             Input = new InputModel
             {
                 RoleList = _roleManager.Roles.Select(x => x.Name).Select(i => new SelectListItem
